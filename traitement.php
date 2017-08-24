badc37e64ee282f5a2ad7f18bb2ba6308238f4bb
Boyquotes <webmaster@linux-live-cd.org>
2017-08-24 17:16:14 +0200

    Correction typo

--- traitement.php
+++ traitement.php
@@ -4,10 +4,10 @@ if($_POST['actionAEffectuer'] == "chercher"){
   echo "Le 20 février 1754, Henri Bernadotte, procureur palois (Béarn), épouse Jeanne de Saint-Jean, originaire de Boeil (Boeil-Bezing) et nièce de l'abbé laïc de Sireix. Le 26 janvier 1763, naît à Pau leur cinquième enfant, Jean-Baptiste1. Celui-ci doit suivre la voie ouverte par son père, et donc étudier le droit. Cependant, il trouve ces études ennuyeuses";
 }
 else if($_POST['actionAEffectuer'] == "eliminer"){
-  echo "Elimination de l'user .$_POST['name']".$_POST['name'];
+  echo "Elimination de l'user ".$_POST['name'];
 }
 else{
   echo "Le plat va être supprimé";
 }
 
-?>
+ ?>
